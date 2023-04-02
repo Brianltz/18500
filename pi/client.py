6b98b00a57c48f4b07b0073baf94bd3d8b6e7fa4
Brian Li <bli@Brians-MacBook-Pro-2.local>
2023-04-02 14:07:36 -0400

    added client code

---
+++ pi/client.py
@@ -0,0 +1,33 @@
+import cv2
+import socket
+import pickle
+import sys
+
+#print(str(sys.argv[1]))
+def main():
+   width = 1280
+   height = 720
+
+   cap = cv2.VideoCapture(-1)
+   cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+   cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+   cap.set(cv2.CAP_PROP_FPS, 10)
+   
+   s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  
+   s.connect((sys.argv[1],8889))
+   
+   try:
+      while(True):
+         ret, frame = cap.read()
+         ret, pack = cv2.imencode('.jpg',frame)
+         packen = pickle.dumps(pack, protocol = 2)
+         print(len(packen))
+         s.sendall(str(len(packen)).zfill(8).encode('ascii'))
+         s.sendall(packen)
+   except Exception as e:
+     print(e)
+     cap.release()
+
+if __name__ == "__main__":
+   main()
